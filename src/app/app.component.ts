443cd70bfee86d0a6e83dc1e2d80c3a448af0b05
Ivan Portugal <ivanbportugal@gmail.com>
2018-03-06 00:02:13 -0600

    boilerplate inputs, pallete, setup. no API yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'bspwa-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bspwa';
+export class AppComponent implements OnInit {
+  title = 'Bible Search';
+  searchText: string = "";
+  searchTextControl = new FormControl();
+  showIntro: boolean = true;
+
+  ngOnInit() {
+    this.searchTextControl.valueChanges
+      .debounceTime(1000)
+      .subscribe(newValue => {
+        console.log('Detected: ' + newValue);
+        if (this.showIntro && newValue.length > 2) {
+          this.showIntro = false;
+        }
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -8,13 +9,27 @@ import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
 
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
